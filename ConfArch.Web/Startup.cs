fd7102e77618e0413f9c5e8f52539722fef99bb3
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-10 16:08:49 +0530

    Add project files.

---
+++ ConfArch.Web/Startup.cs
@@ -0,0 +1,66 @@
+using ConfArch.Data;
+using ConfArch.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConfArch.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(o => o.Filters.Add(new AuthorizeFilter()));        //The Filter makes all Controllers needing Authentication by Default unless specified by AllowAnonymous.
+            services.AddScoped<IConferenceRepository, ConferenceRepository>();
+            services.AddScoped<IProposalRepository, ProposalRepository>();
+            services.AddScoped<IAttendeeRepository, AttendeeRepository>();
+
+            services.AddDbContext<ConfArchDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), 
+                    assembly => assembly.MigrationsAssembly(typeof(ConfArchDbContext).Assembly.FullName)));
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();            //Order is Imp. Should be placed before Endpoints so that it is checked before Endpoints.
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Conference}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
